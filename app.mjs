2827bd89dc730c57753f308f2593ceab21f8f4ab
Lucas Hasting <110124874+LucasHasting@users.noreply.github.com>
2025-08-31 18:21:28 -0500

    added note about 0.0.0.0

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express();
 const PORT = 3000;
-const IP_ADD = '0.0.0.0'; //listen
+const IP_ADD = '0.0.0.0'; //all local addresses
 
 app.get('/', (req, res) => {
   res.send('Hello Express');
